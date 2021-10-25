dec696f7eabfb660ac0b283b9d5063b67d403b2d
talekunci <90287393+talekunci@users.noreply.github.com>
2021-10-25 15:05:13 +0300

    Add files via upload

--- 4.sql
+++ 4.sql
@@ -2,3 +2,8 @@
 alter table projects
 	add column cost integer default 0
 ;
+
+--Значения заглушки
+update projects
+set cost = id * 100
+where cost = 0;
--- 5.sql
+++ 5.sql
@@ -1,7 +1,2 @@
---Значения заглушки
-update projects
-set cost = id * 100
-where cost = 0;
-
 --Cамый дешевый проект (исходя из cost всех проектов)
 select * from projects order by cost limit 1;
